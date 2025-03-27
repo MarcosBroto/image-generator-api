42b95fcf90f4c1ebf35ebf2f370b4984eb9baa4c
Marcos Broto <marcos.broto@movicoders.com>
2025-03-27 17:36:29 +0100

    new version

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import string
-
 from fastapi import FastAPI
 from google.cloud import storage
 import json
@@ -7,15 +6,14 @@ import datetime
 import sys
 import os
 import uvicorn
-from pydantic.v1 import BaseModel
+from pydantic import BaseModel
 from vertexai.preview.vision_models import ImageGenerationModel
 import vertexai
 
 app = FastAPI()
 
-#if not sys.platform.startswith("linux"):
-#    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "/Users/joanenricgarcias/Repositories/gcp-cr_genai-api/skiller-sandbox-ai-b7987e8977bc.json"
-
+if not sys.platform.startswith("linux"):
+    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "C:/Google/image-generator-api-455015-381fd368b2df.json"
 
 # Initialize the Google Cloud Storage client
 storage_client = storage.Client()
@@ -28,36 +26,23 @@ class Prompt(BaseModel):
 @app.post("/generate-image")
 async def generate_image_controller(prompt: Prompt):
     generated_image = generate_image(prompt.description)
-    bytes = generated_image
     blob_name = f"image_{datetime.datetime.utcnow().isoformat()}.jpg"
     blob = bucket.blob(blob_name)
-    blob.upload_from_string(bytes, content_type="image/jpg")
+    blob.upload_from_string(generated_image, content_type="image/jpg")
     return blob.public_url
 
 @app.get("/")
 async def read_root():
-    # The JSON response you want to return and store
     response_json = {"message": "Hello World"}
-
-    # Convert the JSON data to a string and then encode to bytes
     json_bytes = json.dumps(response_json).encode('utf-8')
-
-    # Generate a unique blob name using the current UTC timestamp
     blob_name = f"hello_{datetime.datetime.utcnow().isoformat()}.json"
     blob = bucket.blob(blob_name)
-
-    # Upload the JSON data to the bucket
     blob.upload_from_string(json_bytes, content_type="application/json")
-
-    # Return the JSON response to the client
     return response_json
 
-def generate_image(description: string):
-
+def generate_image(description: str):
     vertexai.init(project="image-generator-api-455015", location="us-central1")
-
     generation_model = ImageGenerationModel.from_pretrained("imagen-3.0-generate-002")
-
     images = generation_model.generate_images(
         prompt=description,
         number_of_images=1,
@@ -67,5 +52,4 @@ def generate_image(description: string):
         safety_filter_level="",
         add_watermark=True,
     )
-
-    return images[0]
+    return images[0].get_bytes()  # Ensure this is the correct way to get image bytes
